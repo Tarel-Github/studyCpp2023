8741870a06ecda914de68700db0ab69f8d3bf12c
Tarel-Github <sms_stmax@naver.com>
2023-03-24 17:29:57 +0900

    3월 24일자 종료

---
+++ baseC/Myclass.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+using namespace std;
+
+class Myclass 
+{
+private:
+	char id;
+	int age;
+	char name[10];
+	//멤버함수(=메소드): 기능
+public:
+	Myclass(char aid, int aage, const char* aname);		// 생성자 원형
+	void getData();
+};
+
+Myclass::Myclass(char aid, int aage, const char* aname) : id(aid), age(aage)
+{
+	printf("생성자 호출\n");
+	//id = aid;
+	//age = aage;
+	strcpy_s(name, 10, aname);
+}
+
+void Myclass::getData()
+{
+	cout << id << ' ' << name << ' ' << age << endl;
+}
+
+int main(void) {
+	Myclass M("aaa", 11);
+}
