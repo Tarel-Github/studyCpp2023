095ee772d96140bb5a4a5e217c3b44f29eb0ed1c
Tarel-Github <sms_stmax@naver.com>
2023-03-28 11:28:51 +0900

    static

--- baseC/ConstObject.cpp
+++ baseC/ConstObject.cpp
@@ -5,14 +5,18 @@ class SoSimple
 {
 private:
 	int num;
+	int num1;
+	const int n = 10;
 public:
-	SoSimple(int n) : num(n) { }
+	SoSimple(int n) : num(n), n(n1) { }
 	SoSimple& AddNum(int n){
 		num += n;
 		return *this;
 	}
 	void ShowData() const {
-		cout << "num: " << num << endl;
+		cout << "num  : " << num << endl;
+		cout << "n    : " << n << endl;
+		cout << "num1 : " << num1 << endl;
 	}
 };
 
---
+++ baseC/MyFriendClass.cpp
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+class Girl;
+
+class Boy
+{
+private:
+	int height;
+	friend class Girl;
+public:
+	Boy(int len) : height(len)
+	{}
+	void ShowYourFriendInfo(Girl& frn);
+};
+
+class Girl
+{
+private:
+	char phNum[20];
+public:
+	Girl(const char* num)
+	{
+		strcpy(phNum, num);
+	}
+	void ShowYourFriendInfo(Boy& frn);
+	friend class Boy;
+};
+
+void Boy::ShowYourFriendInfo(Girl& frn)
+{
+	cout << "Her Phone number: " << frn.phNum << endl;
+}
+
+void Girl::ShowYourFriendInfo(Boy& frn)
+{
+	cout << "His height: " << frn.height << endl;
+}
+
+
+int main(void)
+{
+	Boy boy(170);
+	Girl girl("010-1234-5678");
+	boy.ShowYourFriendInfo(girl);
+	girl.ShowYourFriendInfo(boy);
+	return 0;
+
+}
+
