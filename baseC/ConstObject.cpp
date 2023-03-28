d368dca502386030a4d37d177a68c98296774d67
Tarel-Github <sms_stmax@naver.com>
2023-03-28 09:19:14 +0900

    cpp 테스트

---
+++ baseC/ConstObject.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+
+class SoSimple
+{
+private:
+	int num;
+public:
+	SoSimple(int n) : num(n) { }
+	SoSimple& AddNum(int n){
+		num += n;
+		return *this;
+	}
+	void ShowData() const {
+		cout << "num: " << num << endl;
+	}
+};
+
+int main(void)
+{
+	const SoSimple obj(7);
+	obj.ShowData();
+	return 0;
+}
