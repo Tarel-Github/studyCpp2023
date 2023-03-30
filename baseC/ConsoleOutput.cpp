20240fef4cead3f7c20ce9e3da61dfbd14ca7831
Tarel-Github <sms_stmax@naver.com>
2023-03-30 17:29:33 +0900

    자체제작 콘솔

--- baseC/ConsoleOutput.cpp
+++ baseC/ConsoleOutput.cpp
@@ -2,17 +2,16 @@
 #include <iostream>
 #include <cstring>
 
-
 namespace mystd {
 	using namespace std;
 	class ostream
 	{
 	public:
-		void operator<< (char* str) { printf("%s", str); }
-		void operator<< (char str)	{ printf("%c", str); }
-		void operator<< (int num)	{ printf("%d", num); }
-		void operator<< (double e)	{ printf("%s", e); }
-		void operator<< (ostream& (*fp)(ostream& ostm)) { fp(*this); }
+		void operator<< (const char* str)	{ printf("%s", str); }
+		void operator<< (char str)			{ printf("%c", str); }
+		void operator<< (int num)			{ printf("%d", num); }
+		void operator<< (double e)			{ printf("%g", e); }
+		void operator<< (ostream&	(*fp)(ostream& ostm)) { fp(*this); }
 	};
 	ostream& endl(ostream& ostm) {
 		ostm << '\n';
---
+++ baseC/funpointer.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+
+int add(int n1, int n2){return n1 + n2;}
+int sub(int n1, int n2){return n1 - n2;}
+
+int main()
+{
+	int res;
+	int (*fp)(int, int);	// 함수 포인터 선언
+	fp = add;
+	res =fp(10, 20);
+	printf("%d\n", res);
+	fp = sub;;
+	res = fp(10, 10);
+	printf("d\n", res);
+
+	printf("%p\n", add);
+
+	return 0;
+}
